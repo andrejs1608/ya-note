f72fbfae724dcd49b2dfa5c816297798bf44e747
Andrejs Mordovcevs <andrjuha1608@gmail.com>
2025-09-01 11:49:12 +0200

    com

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -1,20 +1,77 @@
-import pytest
 from http import HTTPStatus
+from pytest_lazy_fixtures import lf
+
+import pytest
 
 from django.urls import reverse
+from pytest_django.asserts import assertRedirects
 
 
-def test_home_availability_for_annonymous_user(client):
-    url = reverse('notes:home')
+@pytest.mark.parametrize(
+    'name',  
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
 
 
 @pytest.mark.parametrize(
     'name',
-    ('notes:home', 'users:login', 'users:signup')
+    ('notes:list', 'notes:add', 'notes:success')
 )
-def test_pages_availability_for_annonymous_user(client, name):
+def test_pages_availability_for_auth_user(admin_client, name):
     url = reverse(name)
-    response = client.get(url)
+    response = admin_client.get(url)
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_author(author_client, name, note):
+    url = reverse(name, args=(note.slug,))
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (lf('admin_client'), HTTPStatus.NOT_FOUND),
+        (lf('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, slug_for_args, expected_status
+):
+    url = reverse(name, args=(slug_for_args))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:detail', lf('slug_for_args')),
+        ('notes:edit', lf('slug_for_args')),
+        ('notes:delete', lf('slug_for_args')),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ),
+)
+# Передаём в тест анонимный клиент, name проверяемых страниц и args:
+def test_redirects(client, name, args):
+    login_url = reverse('users:login')
+    # Теперь не надо писать никаких if и можно обойтись одним выражением.
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
